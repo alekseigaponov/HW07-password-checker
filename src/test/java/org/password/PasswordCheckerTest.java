af22ca62bcbcf3eca2fef85c4df3a862578b8a91
alekseigaponov <aleksei.gaponov@gmail.com>
2024-04-04 21:39:21 +0300

    Created five more test password checker test cases. Created readme file.

--- src/test/java/org/password/PasswordCheckerTest.java
+++ src/test/java/org/password/PasswordCheckerTest.java
@@ -1,5 +1,6 @@
 package org.password;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
@@ -9,4 +10,29 @@ public class PasswordCheckerTest {
     void shouldReturnTrueForComplexPasswordWithDigitsAndSpecialChars() {
          assertTrue( PasswordChecker.checkPasswordComplexity("Password123!") );
     }
+    @Test
+    void testComplexPasswordWithLongLength() {
+        Assertions.assertTrue(PasswordChecker.checkPasswordComplexity("LongPassword1!"));
+    }
+
+    @Test
+    void testComplexPasswordWithDigitsAndSpecialCharsAtStart() {
+        Assertions.assertTrue(PasswordChecker.checkPasswordComplexity("!1Password"));
+    }
+
+    @Test
+    void testComplexPasswordWithDigitsAndSpecialCharsAtEnd() {
+        Assertions.assertTrue(PasswordChecker.checkPasswordComplexity("Password!1"));
+    }
+
+    @Test
+    void testComplexPasswordWithDigitsAndSpecialCharsInTheMiddle() {
+        Assertions.assertTrue(PasswordChecker.checkPasswordComplexity("Pa$$w0rd"));
+    }
+
+    @Test
+    void testComplexPasswordWithRepeatedSpecialChars() {
+        Assertions.assertTrue(PasswordChecker.checkPasswordComplexity("P@$$w0rd!"));
+    }
+
 }
